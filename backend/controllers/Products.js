c1417f04bb2e2ac85802072475653d3d02d937f2
Rhomadon <adonrhomadon100@gmail.com>
2022-03-05 16:17:33 +0700

    Install React

--- backend/controllers/Products.js
+++ backend/controllers/Products.js
@@ -10,3 +10,65 @@ export const getAllProducts = async (req, res) => {
         })
     }
 }
+
+export const getProductById = async (req, res) => {
+    try {
+        const product = await Products.findAll({
+            where: {
+                id: req.params.id
+            }
+        })
+        res.json(product[0])
+    } catch (error) {
+        res.json({
+            message: error.message
+        })
+    }
+}
+
+export const createProduct = async (req, res) => {
+    try {
+        await Products.create(req.body)
+        res.json({
+            "message": "Product Created"
+        })
+    } catch (error) {
+        res.json({
+            message: error.message
+        })
+    }
+}
+
+export const updateProduct = async (req, res) => {
+    try {
+        await Products.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        })
+        res.json({
+            "message": "Product Updated"
+        })
+    } catch (error) {
+        res.json({
+            message: error.message
+        })
+    }
+}
+
+export const deleteProduct = async (req, res) => {
+    try {
+        await Products.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        res.json({
+            "message": "Product Deleted"
+        })
+    } catch (error) {
+        res.json({
+            message: error.message
+        })
+    }
+}
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import db from "./config/database.js"
 import productRoutes from "./routes/index.js"
+import cors from "cors"
 
 const app = express()
 const port = 5000
@@ -12,6 +13,8 @@ try {
     console.error('Unable to connect to the database:', error);
   }
 
+app.use(cors())
+app.use(express.json())
 app.use('/products', productRoutes)
 
 app.listen(port, () => console.log('Server: http://localhost:'+port))
